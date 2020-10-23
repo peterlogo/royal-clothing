c1047cd24e456712b272b5b7bdad1854c628ebe3
Peter Logo <39249358+Together4@users.noreply.github.com>
2020-10-23 02:06:51 -0400

    Update [Collection Item updated]

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { CustomButton } from "../custom-button/custom-button.component";
 import "./collection-item.styles.scss";
 
 export const CollectionItem = ({ id, name, price, imageUrl }) => {
@@ -9,6 +10,9 @@ export const CollectionItem = ({ id, name, price, imageUrl }) => {
         <span className="name">{name}</span>
         <span className="price">{price}</span>
       </div>
+      <CustomButton inverted className="custom-button">
+        ADD TO CART
+      </CustomButton>
     </div>
   );
 };
--- src/components/custom-button/custom-button.component.jsx
+++ src/components/custom-button/custom-button.component.jsx
@@ -1,10 +1,17 @@
 import React from "react";
 import "./custom-button.styles.scss";
 
-export const CustomButton = ({ children, isGoogleSignIn, ...otherProps }) => {
+export const CustomButton = ({
+  children,
+  isGoogleSignIn,
+  inverted,
+  ...otherProps
+}) => {
   return (
     <button
-      className={`${isGoogleSignIn ? "google-sign-in" : ""} custom-button`}
+      className={`${inverted ? "inverted" : ""} ${
+        isGoogleSignIn ? "google-sign-in" : ""
+      } custom-button`}
       {...otherProps}
     >
       {children}
