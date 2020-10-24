f34612ffc9c610d17b3c0ac4f852ebeb6759b112
Peter Logo <39249358+Together4@users.noreply.github.com>
2020-10-24 01:29:25 -0400

    Updates [Redux persist added to persirst state on page referesh]

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -4,10 +4,26 @@
  */
 
 import { combineReducers } from "redux";
+import { persistReducer } from "redux-persist";
 import { userReducer } from "./user/user.reducer";
 import { cartReducer } from "./cart/cart.reducer";
 
-export default combineReducers({
+// get the storage property needed in redux-persist.
+import storage from "redux-persist/lib/storage";
+
+/**
+ * Redux persist configuration
+ * for handling the reducers.
+ */
+const persistConfig = {
+  key: "root",
+  storage,
+  whitelist: ["cart"],
+};
+
+const rootReducer = combineReducers({
   user: userReducer,
   cart: cartReducer,
 });
+
+export default persistReducer(persistConfig, rootReducer);
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,5 +1,6 @@
 import { createStore, applyMiddleware } from "redux";
 import { composeWithDevTools } from "redux-devtools-extension";
+import { persistStore } from "redux-persist";
 import logger from "redux-logger";
 import rootReducer from "./root-reducer";
 
@@ -17,4 +18,11 @@ const store = createStore(
   rootReducer,
   composeWithDevTools(applyMiddleware(...middlewares))
 );
-export default store;
+
+/**
+ * Persits the state values stored in the redux
+ * store.
+ */
+const persistor = persistStore(store);
+
+export { store, persistor };
