7c00cb26d2ec566f36756450cac77d88aaac6e62
Peter Logo <39249358+Together4@users.noreply.github.com>
2020-10-23 20:56:04 -0400

    Updates [Cart Feature completed, enabled items to be added to cart]

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -4,11 +4,11 @@ import { connect } from "react-redux";
 import { toggleCartHidden } from "../../redux/cart/cart.actions";
 import "./cart-icon.styles.scss";
 
-const CartIcon = ({ toggleCartHidden, cartItems }) => {
+const CartIcon = ({ toggleCartHidden, itemCount }) => {
   return (
     <div className="cart-icon" onClick={toggleCartHidden}>
       <ShoppingIcon className="shopping-icon" />
-      <span className="item-count">{cartItems.length}</span>
+      <span className="item-count">{itemCount}</span>
     </div>
   );
 };
@@ -18,7 +18,10 @@ const mapDispatchToProps = (dispatch) => ({
 });
 
 const mapStateToProps = ({ cart: { cartItems } }) => ({
-  cartItems,
+  itemCount: cartItems.reduce(
+    (accumalatedQuantity, cartItem) => accumalatedQuantity + cartItem.quantity,
+    0
+  ),
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(CartIcon);
