eaa644f893bb089ec4c182cddfd0bf88534e3e09
Peter Logo <39249358+Together4@users.noreply.github.com>
2020-11-21 10:46:29 -0500

    Update

--- src/components/stripe-button/stripe-button.component.jsx
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -1,14 +1,26 @@
 import React from "react";
 import StripeCheckout from "react-stripe-checkout";
+import axios from "axios";
 
 const StripeCheckoutButton = ({ price }) => {
   const priceForStripe = price * 100;
   const publishableKey =
     "pk_test_51Hg1HPKlYWb8VjDf1CC8h1Qk51jC7y6gG5htU0oVKwptidXDqa0yoN5xhofDEbcdxuz6VSiwyyP0QbswqkDXetjD00BhYhu6Wp";
-
   const onToken = (token) => {
-    console.log(token);
-    alert("Payment Successful");
+    axios({
+      url: "http://localhost:5000/payment",
+      method: "post",
+      data: { amount: priceForStripe, token },
+    })
+      .then((res) => {
+        alert("Payment Successful");
+      })
+      .catch((error) => {
+        console.log(error);
+        alert(
+          "Their was an issue with your payment. Please make sure you use the provided card."
+        );
+      });
   };
 
   return (
